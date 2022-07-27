7c90641fe15f21b34b4c707572b5362c624895b9
camipony <camibonbu54@gmail.com>
2022-07-27 10:58:11 -0500

    1ero sin PD v.2

--- PuntoA.js
+++ PuntoA.js
@@ -26,7 +26,7 @@ async function input() {
                 let n = parseInt(readLine());//cantidad de lineas (n, primera linea)
                 let p = [];
                 console.log(n);
-                for (let i = 0; i < n; ++i) {
+                for(let i = 0 ; i < n; ++i){
                     let linea = readLine();
                     let data = linea.split(" ");
                     console.log(data);
@@ -57,8 +57,8 @@ async function input() {
 async function output(obj) {
     let out = obj.n + "\n";;
     out += obj.tiempoTotal + "\n";
-    for (let i = 0; i < obj.n; ++i) {
-        out += obj.nombreProcedimientos[i] + "\n";
+    for( let i = 0 ; i < obj.n ; ++i){
+        out+= obj.nombreProcedimientos[i] +"\n";
     }
     return new Promise((resolve, reject) => {
         fs.writeFile(ARCHIVO_ESCRITURA + '.txt', out, (err, list) => {
@@ -75,61 +75,54 @@ async function output(obj) {
 async function solve(n, procedimientos) {
 
     var cantidadProcedimientos = 0;
-    var horaComienzo, horaComienzoNext;//horas y minutos de hi (hora de inicio)
-    var horaLimite = 0; //hora final de la linea anterior
-    var horaFinal = 0, horaFinalNext = 0; //hora final de un procedimiento
-    var tiempoAcum = 0, tiempoAcum2;//Tiempo acumulado (unidad de horas)
+    var horaComienzo;//horas y minutos de hi (hora de inicio)
+    var horaLimite = 0, horaLimite2;//hora final de la linea anterior
+    var tiempoAcum = 0;
+    var tiempoAcum2=0;//Tiempo acumulado (unidad de horas)
     var horasAcum, minutosAcum;
     var nombresProcedimientos = [];
-    var posibilidades = [];
-    var rangos = [];
-
-    //Si un procedimiento consta de 24h es caso base.
-
-    for (var i = 0; i < 1; i++) {
-        horaComienzo = procedimientos[i].horaInicio.hora + (procedimientos[i].horaInicio.minutos / 60);
-        horaFinal = procedimientos[i].horaFin.hora + (procedimientos[i].horaFin.minutos / 60);
-        rangos.push(horaFinal - horaComienzo);
-        for (var j = 1; j < n; j++) {
-            horaComienzoNext = procedimientos[j].horaInicio.hora + (procedimientos[j].horaInicio.minutos / 60);
-            horaFinalNext = procedimientos[j].horaFin.hora + (procedimientos[j].horaFin.minutos / 60);
-            tiempoAcum += (horaFinal - horaComienzo) + (horaFinalNext - horaComienzoNext); //Tiempo en que se usa la sala por un procedimiento    
-            console.log("hora final " + horaFinal + " hora comienzo segundo " + horaComienzoNext);
-
-            if (horaFinal <= horaComienzoNext
-            ) {
-                posibilidades.push(procedimientos[j].nombre);
-                rangos.push(horaFinalNext - horaComienzoNext);
-                //console.log("posibilidades "+posibilidades[j]);
-                //console.log("rangos "+rangos[i]);
-                tiempoAcum = 0;
-                horaComienzoNext = 0;
-                horaFinalNext = 0;
-            } else if (horaComienzo >= horaLimite) {
-                cantidadProcedimientos += 1;
-                console.log(cantidadProcedimientos);
-                console.log(horaComienzo + ">" + horaLimite);
-                horaLimite = procedimientos[j].horaFin.hora + (procedimientos[j].horaFin.minutos / 60);
-                tiempoAcum += horaLimite - horaComienzo;//Tiempo en que se usa la sala por un procedimiento
-                console.log(horaLimite + "-" + horaComienzo);
-                posibilidades.push(procedimientos[j].nombre);//Insertar nombre de procedimientos en un array
-            }
+    var nombresProcedimientos2 = [];
+    
+    for(var i=0;i<n;i++){
+        console.log(procedimientos[i].horaFin.hora);
+        horaComienzo = procedimientos[i].horaInicio.hora+(procedimientos[i].horaInicio.minutos/60);
+        console.log(horaComienzo);
+        if(horaComienzo>=horaLimite){
+            cantidadProcedimientos += 1;
+            console.log(cantidadProcedimientos);
+            console.log(horaComienzo+">"+horaLimite);
+            horaLimite = procedimientos[i].horaFin.hora+(procedimientos[i].horaFin.minutos/60);
+            tiempoAcum += horaLimite-horaComienzo;//Tiempo en que se usa la sala por un procedimiento
+            console.log(horaLimite+"-"+horaComienzo);
+            nombresProcedimientos.push(procedimientos[i].nombre);//Insertar nombre de procedimientos en un array
+        }else if(horaComienzo<=horaLimite){
+            console.log("entra else "+procedimientos[i].nombre);
+            horaLimite2 = procedimientos[i].horaFin.hora+(procedimientos[i].horaFin.minutos/60);
+            tiempoAcum2 += horaLimite2-horaComienzo;//Tiempo en que se usa la sala por un procedimiento
+            nombresProcedimientos2.push(procedimientos[i].nombre);//Insertar nombre de procedimientos en un array
+        }
     }
-}
 
-for (var x = 0; x < posibilidades.length; x++) {
-    console.log(posibilidades[x]);
-    console.log("rangos " + rangos[x])
-}
+    if(tiempoAcum < tiempoAcum2){
+        tiempoAcum = tiempoAcum2;
+        if(nombresProcedimientos.length == nombresProcedimientos2.length){
+            nombresProcedimientos = nombresProcedimientos2;
+        }else{
+            nombresProcedimientos.length = nombresProcedimientos2.length;
+            nombresProcedimientos = nombresProcedimientos2;
+            cantidadProcedimientos = nombresProcedimientos.length;
+        }
+    }
 
+    console.log("console "+nombresProcedimientos2.length)
+    console.log("console1 "+nombresProcedimientos.length)
 
-
-tiempoAcum = (tiempoAcum + "").split(".");
-horasAcum = tiempoAcum[0];
-minutosAcum = Math.trunc(parseFloat(0 + "." + tiempoAcum[1]) * 60);
-console.log("tiempo acum final " + tiempoAcum);
-console.log(minutosAcum);
-return new Respuesta(cantidadProcedimientos, new Hora(horasAcum, minutosAcum), nombresProcedimientos);
+    tiempoAcum = (tiempoAcum+"").split(".");
+    horasAcum = tiempoAcum[0];
+    minutosAcum = Math.trunc(parseFloat(0+"."+tiempoAcum[1])*60);
+    
+    //console.log(minutosAcum);
+    return new Respuesta(cantidadProcedimientos, new Hora(horasAcum, minutosAcum), nombresProcedimientos);
 }
 
 async function main() {
@@ -139,17 +132,17 @@ async function main() {
     await output(res);
 }
 
-function Respuesta(n, tiempoTotal, nombreProcedimientos) {
+function Respuesta(n, tiempoTotal, nombreProcedimientos){
     this.n = n;
     this.tiempoTotal = tiempoTotal;
     this.nombreProcedimientos = nombreProcedimientos;
 }
 
-function Procedimiento(nombre, horaInicio, horaFin) {
+function Procedimiento(nombre, horaInicio, horaFin){
     this.nombre = nombre;
     this.horaInicio = horaInicio;
     this.horaFin = horaFin;
-
+    
 }
 
 class Hora {
@@ -157,17 +150,17 @@ class Hora {
         this.hora = hora;
         this.minutos = minutos;
     }
-    toString() {
+    toString(){
         let res = "";
         if (this.hora < 10)
-            res += "0" + this.hora;
+            res+="0"+this.hora;
         else
-            res += this.hora;
-        res += ":";
+            res+=this.hora; 
+        res+=":";
         if (this.minutos < 10)
-            res += "0" + this.minutos;
+            res+="0"+this.minutos;
         else
-            res += this.minutos;
+            res+=this.minutos;
         return res;
     }
 }
